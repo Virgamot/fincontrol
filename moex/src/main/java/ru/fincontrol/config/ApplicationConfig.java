abba0feeff5bffc8d0d43f634d3ee20d765dae22
virgamot <ipodskochin@mail.ru>
2024-04-21 19:47:32 +0300

    feing clients enabled

--- moex/src/main/java/ru/fincontrol/config/ApplicationConfig.java
+++ moex/src/main/java/ru/fincontrol/config/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package ru.fincontrol.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients
 @EnableConfigurationProperties(MoexConfig.class)
 public class ApplicationConfig {
 }
